b0ee38907462231141273096fcd41f0c95aba972
zsd <124780540@qq.com>
2024-09-06 16:02:38 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="height: 500px;">
+    <VTree :data="data" animation>
+
+      <template v-slot:node="{ node }">
+        <span class="custom-tree-node-2">
+          {{ node.name }}
+        </span>
+      </template>
+
+    </VTree>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VTree from '@wsfe/ctree'
+import treeData from './org.js'
+
+function buildTree(data, root = '') {
+    const map = {}
+    const result = []
+
+    for (let i = 0; i < data.length; i++) {
+        const item = data[i]
+        if (!map[item.id]) {
+            map[item.id] = item
+        }
+    }
+
+    for (let k = 0; k < data.length; k++) {
+        const item = data[k]
+        if (map[item.pid] && map[item.pid] !== root) {
+
+            map[item.pid].children = map[item.pid].children || []
+            map[item.pid].children.push(item)
+        } else {
+            result.push(map[item.id])
+        }
+    }
+
+    return result
+}
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VTree
+  },
+  data() {
+    return {
+      data: []
+    }
+  },
+  mounted() {
+    this.data = Object.freeze(buildTree(treeData))
   }
 }
 </script>
 
 <style>
+@import '~@wsfe/ctree/dist/ctree.css';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
